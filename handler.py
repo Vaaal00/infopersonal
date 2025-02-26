ec7e2dd88f692908477fa790a758fe1e54751d32
Valeria Ceron <valeria.ceron.0499@eam.edu.co>
2025-02-26 13:16:06 -0500

    Commit info extra

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import json
 
 def info(event, context):
     body = {
-        "nombre": "Valeria Ceron", "edad": "19 años"
+        "nombre": "Valeria Ceron", "codigo": "240220232008", "fecha nacimiento": "17/08/2005", "edad": "19 años", "correo": "valeria.ceron.0499@eam.edu.co", "direccion": "Quimbaya, Quindio"
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
